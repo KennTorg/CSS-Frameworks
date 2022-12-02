7b9e844138ee96a3f021b1fe0dc0385ab93d08e9
KennTorg <91532197+KennTorg@users.noreply.github.com>
2022-12-02 08:18:44 +0100

    create/update Post

---
+++ src/js/handlers/updatePost.mjs
@@ -0,0 +1,37 @@
+import { getPost } from "../api/posts/read.mjs";
+import { updatePost } from "../api/posts/update.mjs";
+
+export async function setUpdatePostFormListener() {
+    const form = document.querySelector("#updatePost");
+
+    const url = URL(location.href);
+    const id = url.searchParams.get("id");
+
+    if (form) {
+        const button = form.querySelector("button");
+        button.disabled = true;
+
+        const post = await getPost(id);
+
+        form.title.value = post.title;
+        form.body.value = post.body;
+        form.tags.value = post.tags;
+        form.media.value = post.media;
+
+        button.disabled = false;
+
+        form.addEventListener("submit", async (event) => {
+            event.preventDefault();
+
+            const form = event.target;
+            const formData = new FormData(form);
+            const post = Object.fromEntries(formData.entries());
+            post.id = id;
+
+            // Send to the API
+            await updatePost(post);
+            form.reset();
+            location.href = `/profile/index.html`;
+        });
+    }
+}
