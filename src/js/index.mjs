b8b8a2db70520f020b59e48cb21d5e9bd38e2387
KennTorg <91532197+KennTorg@users.noreply.github.com>
2022-12-02 08:17:01 +0100

    listeners

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,15 +1,40 @@
-import { setRegisterFormListener } from "./handlers/register.mjs";
-
-import { setLoginFormListener } from "./handlers/login.mjs";
-
-setRegisterFormListener();
-
-setLoginFormListener();
+import * as listeners from "./handlers/index.mjs";
+import * as templates from "./templates/index.mjs";
+import * as postMethods from "./api/posts/index.mjs";
 
 const path = location.pathname;
 
-if (path === "/profile/login/") {
-    setLoginFormListener();
-} else if (path === "/profile/register/") {
-    setRegisterFormListener();
+if (path === "/pages/login/") {
+    listeners.setLoginFormListener();
+} else if (path === "/pages/register/") {
+    listeners.setRegisterFormListener();
+} else if (path === "/pages/profile/") {
+    listeners.setCreatePostFormListener;
+} else if (path === "/pages/profile/") {
+    listeners.setUpdatePostFormListener;
 }
+
+/* TESTING SINGLE-POST WORKING***
+
+async function testTemplate() {
+    const posts = await postMethods.getPosts();
+    const post = posts.pop();
+    const container = document.querySelector("#single-post");
+    renderPostTemplate(post, container);
+}
+
+testTemplate();
+
+*/
+
+/* TEST POSTS WORKING***
+
+async function testTemplate() {
+    const posts = await postMethods.getPosts();
+    const container = document.querySelector("#posts");
+    templates.renderPostTemplates(posts, container);
+}
+
+testTemplate();
+
+*/
