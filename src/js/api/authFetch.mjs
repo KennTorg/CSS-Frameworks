a77f808227247695678e539d3acb251de9dad8ee
Kennet Torgersen <91532197+KennTorg@users.noreply.github.com>
2022-12-13 10:20:15 +0100

    auto correct

--- src/js/api/authFetch.mjs
+++ src/js/api/authFetch.mjs
@@ -2,26 +2,27 @@ import { load } from "../storage/index.mjs";
 
 /**
  * Sets the headers.
+ * @returns token
  */
 
 export function headers() {
-    const token = load("token");
+  const token = load("token");
 
-    return {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
-    };
+  return {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  };
 }
 
 /**
  * Authorization fetch for create, read, delete and update.
  * @param {string} url
- * @param {string} options
+ * @param {object} options
  */
 
 export async function authFetch(url, options = {}) {
-    return fetch(url, {
-        ...options,
-        headers: headers()
-    });
+  return fetch(url, {
+    ...options,
+    headers: headers(),
+  });
 }
