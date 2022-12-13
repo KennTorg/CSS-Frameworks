bd1918d6fac5572bb70debb36b8cee4873ae9b8e
Kennet Torgersen <91532197+KennTorg@users.noreply.github.com>
2022-12-13 10:22:14 +0100

    templates

--- src/js/api/posts/read.mjs
+++ src/js/api/posts/read.mjs
@@ -1,5 +1,6 @@
 import { API_SOCIAL_URL } from "../constants.mjs";
 import { authFetch } from "../authFetch.mjs";
+import * as templates from "../../templates/index.mjs";
 
 const action = "/posts";
 const author = "?_author=true";
@@ -9,9 +10,9 @@ const author = "?_author=true";
  */
 
 export async function getPosts() {
-  const updatePostURL = `${API_SOCIAL_URL}${action}`;
+  const getPostsURL = `${API_SOCIAL_URL}${action}${author}`;
 
-  const response = await authFetch(updatePostURL);
+  const response = await authFetch(getPostsURL);
 
   return await response.json();
 }
@@ -19,15 +20,77 @@ export async function getPosts() {
 /**
  * Calling the post with ID from the API.
  * @param {number} id of the the post.
+ * @returns a single post.
  */
 
 export async function getPost(id) {
   if (!id) {
-    throw new Error("Get requires a postID");
+    throw new Error("Requires postID");
   }
-  const getPostURL = `${API_SOCIAL_URL}${action}/${id}`;
+  const getPostURL = `${API_SOCIAL_URL}${action}/${id}${author}`;
 
   const response = await authFetch(getPostURL);
 
   return await response.json();
 }
+
+/**
+ * Displays the posts on the "/profile/
+ */
+export async function postFeed() {
+  const posts = await getPosts();
+  const container = document.querySelector("#posts");
+  templates.renderPostTemplates(posts, container);
+}
+
+/**
+ * Function that displays single post.
+ */
+export async function singlePost() {
+  const queryString = document.location.search;
+  const params = new URLSearchParams(queryString);
+  const id = params.get("id");
+
+  const post = await getPost(id);
+  const singlePostContainer = document.querySelector("#single-post");
+  templates.renderPostTemplate(post, singlePostContainer);
+}
+
+/**
+ * Function that displays the edited posts.
+ */
+/*
+export async function displayEditedPost() {
+  const queryString = document.location.search;
+  const params = new URLSearchParams(queryString);
+  const id = params.get("id");
+
+  const post = await getPost(id);
+  const singlePostContainer = document.querySelector("#");
+  renderPostTemplates.renderPostTemplate(post, singlePostContainer);
+}
+*/
+
+/**
+ * The function that gets the searched posts.
+ */
+
+/*
+export async function getSearchedPosts() {
+  const posts = await getPosts();
+  const container = document.querySelector("#posts");
+  renderPostTemplates.renderSearchedPosts(posts, container);
+}
+*/
+
+/**
+ * The function that get the filtered posts.
+ */
+
+/*
+export async function getFilteredPosts() {
+  const posts = await postFilter();
+  const container = document.querySelector("#posts");
+  renderPostTemplates.renderFilteredPosts(posts, container);
+}
+*/
--- src/js/templates/post.mjs
+++ src/js/templates/post.mjs
@@ -1,12 +1,102 @@
+import * as posts from "../api/posts/index.mjs";
+import { load } from "../storage/index.mjs";
+
+/**
+ * Creates HTML for list of posts on /pages/profile/.
+ * @param {string} postData
+ */
+
 export function postTemplate(postData) {
-  // const { author, id } = postData;
-  // const { name, avatar } = author;
+  const { author, id } = postData;
+  const { name, avatar } = author;
 
   const post = document.createElement("div");
   post.classList = "card mt-3";
 
-  // POST HEADER ************ wait till avatar and banner is called using the edit profile! ******
-  /*
+  // Post Header ************ wait till avatar and banner is called using the edit profile! ******
+
+  const postHeader = document.createElement("div");
+  postHeader.classList = "d-flex justify-content-between p-2 px-3";
+  post.append(postHeader);
+
+  const user = document.createElement("div");
+  user.classList = "d-flex flex-row ml-2 p-2";
+
+  const postAuthor = document.createElement("span");
+  postAuthor.classList = "font-weight-bold";
+  postAuthor.innerText = name;
+
+  if (avatar) {
+    const userAvatar = document.createElement("img");
+    userAvatar.classList = "rounded-circle";
+    userAvatar.src = avatar;
+    userAvatar.alt = `${name}s Avatar`;
+    userAvatar.width = "50";
+    userAvatar.height = "50";
+    user.append(userAvatar, postAuthor);
+    postHeader.append(user);
+  } else {
+    const userAvatar = document.createElement("img");
+    userAvatar.classList = "rounded-circle";
+    userAvatar.src = "https://picsum.photos/seed/picsum/200/300";
+    userAvatar.alt = `default Avatar`;
+    userAvatar.width = "50";
+    userAvatar.height = "50";
+    user.append(userAvatar, postAuthor);
+    postHeader.append(user);
+  }
+
+  if (postData.media) {
+    const img = document.createElement("img");
+    img.src = postData.media;
+    img.alt = `Image from ${postData.title}`;
+    post.append(img);
+  }
+
+  // Post body
+  const postBodyContainer = document.createElement("div");
+  postBodyContainer.classList = "card-body";
+  post.append(postBodyContainer);
+
+  const postTitle = document.createElement("h4");
+  postTitle.classList = "card-text";
+  postTitle.innerText = postData.title;
+  postBodyContainer.append(postTitle);
+
+  const postBody = document.createElement("p");
+  postBody.classList = "card-text";
+  postBody.innerText = postData.body;
+  postBodyContainer.append(postBody);
+
+  const viewPost = document.createElement("a");
+  viewPost.classList = "d-flex m-3 me-5";
+  viewPost.setAttribute("href", `/pages/post/index.html`);
+  viewPost.innerHTML = "View Post";
+  postBodyContainer.append(viewPost);
+
+  return post;
+}
+
+/**
+ * Creates HTML for a single post on /pages/post/.
+ * @param {string} postData
+ */
+
+export function singlePostTemplate(postData) {
+  const { author, id } = postData;
+  const { name, avatar } = author;
+
+  const singlePost = document.querySelector(".singlePostName");
+  const path = location.pathname;
+  if (path === "/pages/post/") {
+    singlePost.innerText = name;
+  }
+
+  const post = document.createElement("div");
+  post.classList = "card mt-3";
+
+  // Post Header
+
   const postHeader = document.createElement("div");
   postHeader.classList = "d-flex justify-content-between p-2 px-3";
   post.append(postHeader);
@@ -35,13 +125,6 @@ export function postTemplate(postData) {
     user.append(userAvatar, postAuthor);
     postHeader.append(user);
   }
-  */
-
-  /*
-  const post = document.createElement("div");
-  post.classList.add("post");
-  post.innerText = postData.title;
-  */
 
   if (postData.media) {
     const img = document.createElement("img");
@@ -50,7 +133,7 @@ export function postTemplate(postData) {
     post.append(img);
   }
 
-  // POST BODY
+  // Post body
   const postBodyContainer = document.createElement("div");
   postBodyContainer.classList = "card-body";
   post.append(postBodyContainer);
@@ -65,17 +148,11 @@ export function postTemplate(postData) {
   postBody.innerText = postData.body;
   postBodyContainer.append(postBody);
 
-  const viewPost = document.createElement("a");
-  viewPost.classList = "d-flex m-3 me-5";
-  viewPost.setAttribute("href", `/pages/post/index.html`);
-  viewPost.innerHTML = "View Post";
-  postBodyContainer.append(viewPost);
-
   return post;
 }
 
 export function renderPostTemplate(postData, parent) {
-  parent.append(postTemplate(postData));
+  parent.append(singlePostTemplate(postData));
 }
 
 export function renderPostTemplates(postDataList, parent) {
