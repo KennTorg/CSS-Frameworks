0b974ec551a00f7daef37dbc13d3806be0bf64a3
Kennet Torgersen <91532197+KennTorg@users.noreply.github.com>
2022-12-13 10:22:42 +0100

    minor changes

--- src/js/handlers/createPost.mjs
+++ src/js/handlers/createPost.mjs
@@ -1,17 +1,21 @@
 import { createPost } from "../api/posts/create.mjs";
 
+/**
+ * Listener for create post.
+ */
+
 export function setCreatePostFormListener() {
-    const form = document.querySelector("#createPost");
+  const form = document.querySelector("#createPost");
 
-    if (form) {
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-            const form = event.target;
-            const formData = new FormData(form);
-            const post = Object.fromEntries(formData.entries());
+  if (form) {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const formData = new FormData(form);
+      const post = Object.fromEntries(formData.entries());
 
-            // Send to the API
-            createPost(post);
-        });
-    }
+      // Send to the API
+      createPost(post);
+    });
+  }
 }
